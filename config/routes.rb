75e7713f6ace35a32b6074b10ce556519db49d8f
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-13 12:41:30 +0200

    Jsx Main has created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'administration/index', :to => 'administration#index'
+
+  root to: 'home#index'
+  
+  resources :administration
+
+  namespace :api do
+    namespace :v1 do
+     resources :blog
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
